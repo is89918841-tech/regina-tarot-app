397f40711832d536b2d4ad1c104b1cd7036d71b6
is89918841-tech <is89918841@gmail.com>
2026-04-15 02:31:18 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const OpenAI = require("openai");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 const openai = new OpenAI({
