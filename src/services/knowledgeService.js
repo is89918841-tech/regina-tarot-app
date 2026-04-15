65268d74d1b81def42b8a2543263dfc22f8a7130
is89918841-tech <is89918841@gmail.com>
2026-04-16 01:34:50 +0900

    Update knowledgeService.js

--- src/services/knowledgeService.js
+++ src/services/knowledgeService.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const fs = require('fs/promises');
+const fs = require('fs')
+const fsp = require('fs').promises
 const OpenAI = require('openai');
 const env = require('../config/env');
 const { ensureDir, readJson, writeJson } = require('../utils/fileStore');
