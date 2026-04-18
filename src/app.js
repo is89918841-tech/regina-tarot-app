ed5cbffb4c8288dc256f2d2a48fbe2851da74404
is89918841-tech <is89918841@gmail.com>
2026-04-19 01:52:44 +0900

    Remove duplicate import of adminRoutes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { ensureDir } = require('./utils/fileStore');
 const env = require('./config/env');
 const adminRoutes = require('./routes/adminRoutes');
-const adminRoutes = require('./routes/adminRoutes');
 const readingRoutes = require('./routes/readingRoutes');
 const consultationRoutes = require('./routes/consultationRoutes'); // 🔥 이거 추가
 const adminAuth = require('./middleware/adminAuth');
