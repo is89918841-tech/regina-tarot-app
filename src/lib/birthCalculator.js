9c70b1b1c23c088c752088f6ea3b9031ffb78503
is89918841-tech <is89918841@gmail.com>
2026-05-07 23:46:53 +0900

    Add functions to build contexts and profiles

--- src/lib/birthCalculator.js
+++ src/lib/birthCalculator.js
@@ -1,5 +1,20 @@
 const { calculate } = require('./combinedAnalysis');
 
+function buildLotteryContext(input) {
+  return calculate(input, { mode: 'lottery' });
+}
+
+function buildBirthProfile(input) {
+  return calculate(input, { mode: 'profile' });
+}
+
+function buildReadingContext(input) {
+  return calculate(input, { mode: 'reading' });
+}
+
 module.exports = {
   calculate,
+  buildLotteryContext,
+  buildBirthProfile,
+  buildReadingContext,
 };
